50cc39ab05150eec991e9c90927fe76c8420f898
Mahfod <mahfod.dev@gmail.com>
2023-01-09 20:06:18 +0100

    Create RestFul Products and Search

---
+++ pages/api/search/[q].ts
@@ -0,0 +1,44 @@
+import { IProduct } from './../../../interfaces/products';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { db } from '../../../database';
+import { Product } from '../../../database/models';
+
+type Data =
+	| {
+			message: string;
+	  }
+	| IProduct[];
+
+export default function handler(
+	req: NextApiRequest,
+	res: NextApiResponse<Data>
+) {
+	switch (req.method) {
+		case 'GET':
+			return searchProducts(req, res);
+
+		default:
+			return res.status(400).json({ message: 'Bad request' });
+	}
+}
+async function searchProducts(req: NextApiRequest, res: NextApiResponse<Data>) {
+	let { q = '' } = req.query;
+	if (q.length === 0) {
+		return res
+			.status(400)
+			.json({ message: 'Vous devez spécifier la query de recherche' });
+	}
+	q = q.toString().trim().toLowerCase();
+
+	await db.connect();
+
+	const products = await Product.find({
+		$text: { $search: q },
+	})
+		.select('title images price inStock slug -_id')
+		.lean();
+
+	await db.disconnect();
+
+	return res.status(200).json(products);
+}
